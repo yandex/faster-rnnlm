yandex/faster-rnnlm

commit ce038058488a17a34e069e23f71aa5b61e0fc3bf

Refactor inner layer classes

faster-rnnlm/layers/activation_functions.h
@@ -1,0 +1,86 @@
+#ifndef FASTER_RNNLM_LAYERS_ACTIVATION_FUNCTIONS_H_
+#define FASTER_RNNLM_LAYERS_ACTIVATION_FUNCTIONS_H_
+
+#include <math.h>
+
+#include "faster-rnnlm/settings.h"
+
+
+namespace {
+const Real kReLUTruncation = 20;
+};
+
+
+// For each activation function a class with two static methods must be defined,
+// namely Forward and Backward
+//
+// Forward applies the function f to each elemente in the given array
+// Backward takes f(x) as the first argument and multiples elements
+// of the second array by f'(x)
+struct IActivation {
+  virtual void Forward(Real* hidden, int size) = 0;
+  virtual void Backward(const Real* hidden, int size, Real* hidden_g) = 0;
+  virtual ~IActivation() {}
+};
+
+
+struct SigmoidActivation : public IActivation {
+  void Forward(Real* hidden, int size) {
+    for (int i = 0; i < size; i++) {
+      hidden[i] = exp(hidden[i]) / (1 + exp(hidden[i]));
+    }
+  }
+
+  void Backward(const Real* hidden, int size, Real* hidden_g) {
+    for (int i = 0; i < size; ++i) {
+      hidden_g[i] *= hidden[i] * (1 - hidden[i]);
+    }
+  }
+};
+
+
+struct TanhActivation : public IActivation {
+  void Forward(Real* hidden, int size) {
+    for (int i = 0; i < size; i++) {
+      hidden[i] = tanh(hidden[i]);
+    }
+  }
+
+  void Backward(const Real* hidden, int size, Real* hidden_g) {
+    for (int i = 0; i < size; ++i) {
+      hidden_g[i] *= (1 - hidden[i] * hidden[i]);
+    }
+  }
+};
+
+
+struct ReLUActivation : public IActivation {
+  void Forward(Real* hidden, int size) {
+    for (int i = 0; i < size; i++) {
+      hidden[i] = (hidden[i] > 0) ? hidden[i] : 0;
+    }
+  }
+
+  void Backward(const Real* hidden, int size, Real* hidden_g) {
+    for (int i = 0; i < size; ++i) {
+      hidden_g[i] *= static_cast<int>(hidden[i] > 0);
+    }
+  }
+};
+
+
+struct TruncatedReLUActivation : public IActivation {
+  void Forward(Real* hidden, int size) {
+    for (int i = 0; i < size; i++) {
+      hidden[i] = (hidden[i] > 0 && hidden[i] < kReLUTruncation) ? hidden[i] : 0;
+    }
+  }
+
+  void Backward(const Real* hidden, int size, Real* hidden_g) {
+    for (int i = 0; i < size; ++i) {
+      hidden_g[i] *= static_cast<int>(hidden[i] > 0 && hidden[i] < kReLUTruncation);
+    }
+  }
+};
+
+#endif  // FASTER_RNNLM_LAYERS_ACTIVATION_FUNCTIONS_H_

build.sh
@@ -15,7 +15,7 @@ dst_folder="eigen3"
 
 function wget_or_curl() {
   [ $# -eq 2 ] || { echo "Usage: wget_or_curl <url> <fpath>" && exit 1; }
-  if type wgt &> /dev/null; then
+  if type wget &> /dev/null; then
     local download_cmd="wget -T 10 -t 3 -O"
   else
     local download_cmd="curl -L -o"

faster-rnnlm/Makefile
@@ -27,7 +27,8 @@ else
 	LDFLAGS += -lrt
 endif
 
-OBJ_FILES = rnnlm.o hierarchical_softmax.o nce.o words.o maxent.o nnet.o recurrent.o
+LAYER_OBJ_FILES = layers/simple_layer.o layers/gru_layer.o layers/scrn_layer.o layers/layer_stack.o layers/interface.o
+OBJ_FILES = hierarchical_softmax.o nce.o words.o maxent.o nnet.o $(LAYER_OBJ_FILES)
 ifeq ($(NVCC_TEST), nvcc)
 	OBJ_FILES += cuda_softmax.o
 endif
@@ -43,26 +44,38 @@ hierarchical_softmax.o : hierarchical_softmax.cc hierarchical_softmax.h maxent.h
 words.o : words.cc words.h settings.h
 	$(CC) $< -c -o $@ $(CFLAGS)
 
-rnnlm.o : rnnlm.cc maxent.h nnet.h nce.h settings.h hierarchical_softmax.h words.h recurrent.h util.h program_options.h
+rnnlm.o : rnnlm.cc maxent.h nnet.h nce.h settings.h hierarchical_softmax.h words.h layers/interface.h util.h program_options.h
 	$(CC) $< -c -o $@ $(CFLAGS)
 
-nce.o : nce.cc nce.h cuda_softmax.h maxent.h recurrent.h settings.h words.h util.h
+nce.o : nce.cc nce.h cuda_softmax.h maxent.h layers/interface.h settings.h words.h util.h
 	$(CC) $< -c -o $@ $(CFLAGS)
 
 maxent.o : maxent.cc maxent.h settings.h
 	$(CC) $< -c -o $@ $(CFLAGS)
 
-recurrent.o : recurrent.cc recurrent.h settings.h util.h
-	$(CC) $< -c -o $@ $(CFLAGS)
-
-nnet.o : nnet.cc nnet.h maxent.h settings.h hierarchical_softmax.h words.h recurrent.h util.h
+nnet.o : nnet.cc nnet.h maxent.h settings.h hierarchical_softmax.h words.h layers/interface.h util.h
 	$(CC) $< -c -o $@ $(CFLAGS)
 
 cuda_softmax.o : cuda_softmax.cu cuda_softmax.h settings.h
 	nvcc $< -c -Xcompiler "$(NVCC_CFLAGS)" -o $@
 
-tests : tests.cc $(OBJ_FILES)
-	$(CC) $^ -o $@ $(CFLAGS) $(LDFLAGS)
+layers/simple_layer.o : layers/simple_layer.cc layers/simple_layer.h layers/interface.h layers/activation_functions.h layers/util.h
+	$(CC) $< -c -o $@ $(CFLAGS)
+
+layers/scrn_layer.o : layers/scrn_layer.cc layers/scrn_layer.h layers/interface.h layers/util.h layers/activation_functions.h settings.h util.h
+	$(CC) $< -c -o $@ $(CFLAGS)
+
+layers/gru_layer.o : layers/gru_layer.cc layers/gru_layer.h layers/interface.h layers/util.h layers/activation_functions.h settings.h util.h
+	$(CC) $< -c -o $@ $(CFLAGS)
+
+layers/layer_stack.o : layers/layer_stack.cc layers/layer_stack.h layers/interface.h layers/util.h settings.h util.h
+	$(CC) $< -c -o $@ $(CFLAGS)
+
+layers/interface.o : layers/interface.cc layers/interface.h layers/util.h settings.h util.h $(LAYER_OBJ_FILES:.o=.h)
+	$(CC) $< -c -o $@ $(CFLAGS)
+
+test_gradients/test_gradients : test_gradients/test_gradients.cc $(OBJ_FILES)
+	$(CC) $^ -o $@ -std=gnu++0x $(CFLAGS) $(LDFLAGS)
 
 clean:
 	rm -f rnnlm $(OBJ_FILES)

faster-rnnlm/layers/gru_layer.cc
@@ -1,0 +1,191 @@
+#include "faster-rnnlm/layers/gru_layer.h"
+
+#include "faster-rnnlm/layers/activation_functions.h"
+
+
+class GRULayer::Updater : public IRecUpdater, public TruncatedBPTTMixin<GRULayer::Updater> {
+ public:
+  explicit Updater(GRULayer* layer)
+      : IRecUpdater(layer->weights_.layer_size_)
+      , layer_(*layer)
+      , reset_(MAX_SENTENCE_WORDS, size_), reset_g_(reset_)
+      , update_(MAX_SENTENCE_WORDS, size_), update_g_(update_)
+      , partialhidden_(MAX_SENTENCE_WORDS, size_), partialhidden_g_(partialhidden_)
+      , quasihidden_(MAX_SENTENCE_WORDS, size_), quasihidden_g_(quasihidden_)
+
+      , syn_reset_in_(&layer_.weights_.syn_reset_in_)
+      , syn_reset_out_(&layer_.weights_.syn_reset_out_)
+      , syn_update_in_(&layer_.weights_.syn_update_in_)
+      , syn_update_out_(&layer_.weights_.syn_update_out_)
+      , syn_quasihidden_in_(&layer_.weights_.syn_quasihidden_in_)
+      , syn_quasihidden_out_(&layer_.weights_.syn_quasihidden_out_)
+
+      , bias_reset_(&layer_.weights_.bias_reset_)
+      , bias_update_(&layer_.weights_.bias_update_)
+    {
+        partialhidden_.row(0).setZero();
+        reset_g_.row(0).setZero();
+    }
+
+  void ForwardSubSequence(int start, int steps);
+
+  void BackwardSequence(int steps, uint32_t truncation_seed, int bptt_period, int bptt);
+
+  void UpdateWeights(int steps, Real lrate, Real l2reg, Real rmsprop, Real gradient_clipping);
+
+  void BackwardStep(int step);
+
+  void BackwardStepThroughTime(int step);
+
+ private:
+  GRULayer& layer_;
+
+  RowMatrix reset_, reset_g_;
+  RowMatrix update_, update_g_;
+  RowMatrix partialhidden_, partialhidden_g_;
+  RowMatrix quasihidden_, quasihidden_g_;
+
+  WeightMatrixUpdater<RowMatrix> syn_reset_in_;
+  WeightMatrixUpdater<RowMatrix> syn_reset_out_;
+  WeightMatrixUpdater<RowMatrix> syn_update_in_;
+  WeightMatrixUpdater<RowMatrix> syn_update_out_;
+  WeightMatrixUpdater<RowMatrix> syn_quasihidden_in_;
+  WeightMatrixUpdater<RowMatrix> syn_quasihidden_out_;
+
+  WeightMatrixUpdater<RowVector> bias_reset_;
+  WeightMatrixUpdater<RowVector> bias_update_;
+};
+
+void GRULayer::Updater::ForwardSubSequence(int start, int steps) {
+  reset_.middleRows(start, steps) = input_.middleRows(start, steps);
+  update_.middleRows(start, steps) = input_.middleRows(start, steps);
+  quasihidden_.middleRows(start, steps) = input_.middleRows(start, steps);
+
+  if (layer_.use_input_weights_) {
+    reset_.middleRows(start, steps) *= syn_reset_in_.W().transpose();
+    update_.middleRows(start, steps) *= syn_update_in_.W().transpose();
+    quasihidden_.middleRows(start, steps) *= syn_quasihidden_in_.W().transpose();
+  }
+
+  for (int step = start; step < start + steps; ++step) {
+    if (layer_.use_bias_) {
+      reset_.row(step) += bias_reset_.W();
+      update_.row(step) += bias_update_.W();
+    }
+
+    if (step != 0) {
+      reset_.row(step).noalias()  += output_.row(step - 1) * syn_reset_out_.W().transpose();
+      update_.row(step).noalias() += output_.row(step - 1) * syn_update_out_.W().transpose();
+    }
+    SigmoidActivation().Forward(reset_.row(step).data(), size_);
+    SigmoidActivation().Forward(update_.row(step).data(), size_);
+
+    if (step != 0) {
+      partialhidden_.row(step).noalias() = output_.row(step - 1).cwiseProduct(reset_.row(step));
+      quasihidden_.row(step).noalias() +=
+          partialhidden_.row(step) * syn_quasihidden_out_.W().transpose();
+    }
+    TanhActivation().Forward(quasihidden_.row(step).data(), size_);
+
+    if (step == 0) {
+      output_.row(step).row(step).noalias()
+          = quasihidden_.row(step).cwiseProduct(update_.row(step));
+    } else {
+      // these 3 lines means:
+      // output_t = (quasihidden_t - output_{t - 1}) * update_t + output_{t - 1}
+      output_.row(step).noalias() = quasihidden_.row(step) - output_.row(step - 1);
+      output_.row(step).array() *= update_.row(step).array();
+      output_.row(step) += output_.row(step - 1);
+    }
+  }
+}
+
+void GRULayer::Updater::BackwardSequence(
+    int steps, uint32_t truncation_seed, int bptt_period, int bptt) {
+  BackwardSequenceTruncated(steps, truncation_seed, bptt_period, bptt);
+
+  input_g_.topRows(steps).setZero();
+  if (layer_.use_input_weights_) {
+    input_g_.topRows(steps).noalias() += quasihidden_g_.topRows(steps) * syn_update_in_.W();
+    input_g_.topRows(steps).noalias() += reset_g_.topRows(steps) * syn_reset_in_.W();
+    input_g_.topRows(steps).noalias() += update_g_.topRows(steps) * syn_update_in_.W();
+  } else {
+    input_g_.topRows(steps) += quasihidden_g_.topRows(steps);
+    input_g_.topRows(steps) += reset_g_.topRows(steps);
+    input_g_.topRows(steps) += update_g_.topRows(steps);
+  }
+}
+
+void GRULayer::Updater::BackwardStep(int step) {
+  update_g_.row(step) = quasihidden_.row(step);
+  if (step != 0) {
+    update_g_.row(step) -= output_.row(step - 1);
+  }
+  update_g_.row(step).array() *= output_g_.row(step).array();
+  SigmoidActivation().Backward(update_.row(step).data(), size_, update_g_.row(step).data());
+
+  quasihidden_g_.row(step) = output_g_.row(step).cwiseProduct(update_.row(step));
+  TanhActivation().Backward(quasihidden_.row(step).data(), size_, quasihidden_g_.row(step).data());
+
+  partialhidden_g_.row(step).noalias() = quasihidden_g_.row(step) * syn_quasihidden_out_.W();
+
+  if (step != 0) {
+    reset_g_.row(step) = partialhidden_g_.row(step).cwiseProduct(output_.row(step - 1));
+  }
+  SigmoidActivation().Backward(reset_.row(step).data(), size_, reset_g_.row(step).data());
+}
+
+
+void GRULayer::Updater::BackwardStepThroughTime(int step) {
+  // these 2 lines means: h'_{t - 1} += (1 - u_t) * h'_t
+  output_g_.row(step - 1) += output_g_.row(step);
+  output_g_.row(step - 1) -= update_.row(step).cwiseProduct(output_g_.row(step));
+
+  output_g_.row(step - 1) += reset_.row(step).cwiseProduct(partialhidden_g_.row(step));
+  output_g_.row(step - 1).noalias() += reset_g_.row(step) * syn_reset_out_.W();
+  output_g_.row(step - 1).noalias() += update_g_.row(step) * syn_update_out_.W();
+}
+
+
+void GRULayer::Updater::UpdateWeights(
+    int steps, Real lrate, Real l2reg, Real rmsprop, Real gradient_clipping) {
+  if (steps <= 1 || size_ == 0) {
+    return;
+  }
+
+  UpdateRecurrentSynWeights(steps - 1, lrate, l2reg, rmsprop, gradient_clipping,
+      partialhidden_.middleRows(1, steps - 1), quasihidden_g_.middleRows(1, steps - 1),
+      &syn_quasihidden_out_);
+  UpdateRecurrentSynWeights(steps - 1, lrate, l2reg, rmsprop, gradient_clipping,
+      output_, reset_g_.bottomRows(reset_g_.rows() - 1),
+      &syn_reset_out_);
+  UpdateRecurrentSynWeights(steps - 1, lrate, l2reg, rmsprop, gradient_clipping,
+      output_, update_g_.bottomRows(update_g_.rows() - 1),
+      &syn_update_out_);
+
+  if (layer_.use_input_weights_) {
+    UpdateRecurrentSynWeights(steps, lrate, l2reg, rmsprop, gradient_clipping,
+        input_, quasihidden_g_,
+        &syn_quasihidden_in_);
+    UpdateRecurrentSynWeights(steps, lrate, l2reg, rmsprop, gradient_clipping,
+        input_, reset_g_,
+        &syn_reset_in_);
+    UpdateRecurrentSynWeights(steps, lrate, l2reg, rmsprop, gradient_clipping,
+        input_, update_g_,
+        &syn_update_in_);
+  }
+
+  if (layer_.use_bias_) {
+    *bias_reset_.GetGradients() = reset_g_.middleRows(1, steps - 1).colwise().mean();
+    bias_reset_.ApplyGradients(lrate, l2reg, rmsprop, gradient_clipping);
+
+    *bias_update_.GetGradients() = update_g_.middleRows(1, steps - 1).colwise().mean();
+    bias_update_.ApplyGradients(lrate, l2reg, rmsprop, gradient_clipping);
+  }
+}
+
+
+IRecUpdater* GRULayer::CreateUpdater() {
+  return new Updater(this);
+}
+

faster-rnnlm/layers/gru_layer.h
@@ -1,0 +1,94 @@
+#ifndef FASTER_RNNLM_LAYERS_GRU_LAYER_H_
+#define FASTER_RNNLM_LAYERS_GRU_LAYER_H_
+
+#include <stdio.h>
+
+#include "faster-rnnlm/layers/interface.h"
+#include "faster-rnnlm/layers/util.h"
+#include "faster-rnnlm/settings.h"
+#include "faster-rnnlm/util.h"
+
+// Gated Recurrent Unit
+// http://arxiv.org/pdf/1412.3555.pdf
+//
+// The unit could be defined using the following parameters:
+//   x_{t} = input_.row(t)          - input vector
+//   u_{t} = update_.row(t)         - update gate
+//   r_{t} = reset_.row(t)          - reset gate
+//   p_{t} = partialhidden_.row(t)  - previous hidden after reset
+//   q_{t} = quasihidden_.row(t)    - hidden state candidate
+//   h_{t} = output.row(t)          - actual hidden state (output vector)
+//
+// Matrices:
+//   I^{u} = syn_update_in_          W^{u} = syn_update_out_
+//   I^{r} = syn_reset_in_           W^{r} = syn_reset_out_
+//   I^{q} = syn_quasihidden_in_     W^{q} = syn_quasihidden_out_
+// Vectors:
+//   b^{u} = bias_update_
+//   b^{r} = bias_reset_
+//
+// The following constrains hold:
+//  h_{-1} = 0 (vector of zeros)
+//  u_{t} = \sigma(I^u x_t + W^u h_{t - 1} + b^u)
+//  r_{t} = \sigma(I^u x_t + W^r h_{t - 1} + b^r)
+//  p_{t} = r_t .*. h_{t - 1}   (elementwise multiplication)
+//  q_{t} = tanh(I^q x_t + W^q p_{t})
+//  h_{t} = h_{t - 1} .*. (1 - u_t) +  q_{t} .*. u_t
+
+class GRULayer : public IRecLayer {
+ public:
+  class Updater;
+
+  class Weights : public IStdWeights {
+   public:
+    explicit Weights(int layer_size)
+        :IStdWeights(6, 2, layer_size)
+        , syn_reset_in_(matrices_[0])
+        , syn_reset_out_(matrices_[1])
+        , syn_update_in_(matrices_[2])
+        , syn_update_out_(matrices_[3])
+        , syn_quasihidden_in_(matrices_[4])
+        , syn_quasihidden_out_(matrices_[5])
+        , bias_reset_(vectors_[0])
+        , bias_update_(vectors_[1])
+    {
+      // initialize input synapses with identity matrices
+      syn_reset_in_.setIdentity();
+      syn_update_in_.setIdentity();
+      syn_quasihidden_in_.setIdentity();
+    }
+
+    void DiagonalInitialization(Real alpha) {}
+
+   protected:
+    RowMatrix& syn_reset_in_;
+    RowMatrix& syn_reset_out_;
+    RowMatrix& syn_update_in_;
+    RowMatrix& syn_update_out_;
+    RowMatrix& syn_quasihidden_in_;
+    RowMatrix& syn_quasihidden_out_;
+
+    RowVector& bias_reset_;
+    RowVector& bias_update_;
+
+    friend class Updater;
+  };
+
+  GRULayer(int layer_size, bool use_bias, bool use_input_weights)
+    : weights_(layer_size), use_bias_(use_bias) , use_input_weights_(use_input_weights) {}
+
+  IRecUpdater* CreateUpdater();
+
+  IRecWeights* GetWeights() { return &weights_; }
+
+ private:
+  friend class Updater;
+
+  Weights weights_;
+  const bool use_bias_;
+  const bool use_input_weights_;
+};
+
+#endif  // FASTER_RNNLM_LAYERS_GRU_LAYER_H_
+
+

faster-rnnlm/layers/interface.cc
@@ -1,0 +1,81 @@
+#include "faster-rnnlm/layers/interface.h"
+
+#include "faster-rnnlm/layers/gru_layer.h"
+#include "faster-rnnlm/layers/layer_stack.h"
+#include "faster-rnnlm/layers/scrn_layer.h"
+#include "faster-rnnlm/layers/simple_layer.h"
+
+
+IRecLayer* CreateSingleLayer(const std::string& layer_type, int layer_size, bool first_layer) {
+  // SimpleRecurrentLayer arguments: (int size, bool use_input_weights, IActivation*)
+  if (layer_type == "sigmoid") {
+    return new SimpleRecurrentLayer(layer_size, !first_layer, new SigmoidActivation());
+  }
+  if (layer_type == "tanh") {
+    return new SimpleRecurrentLayer(layer_size, !first_layer, new TanhActivation());
+  }
+  if (layer_type == "relu") {
+    return new SimpleRecurrentLayer(layer_size, !first_layer, new ReLUActivation());
+  }
+  if (layer_type == "relu-trunc") {
+    return new SimpleRecurrentLayer(layer_size, !first_layer, new TruncatedReLUActivation());
+  }
+  // GRULayer arguments: (int size, bool use_bias, bool use_input_weights)
+  if (layer_type == "gru") {
+    return new GRULayer(layer_size, false, false);
+  }
+  if (layer_type == "gru-bias") {
+    return new GRULayer(layer_size, true, false);
+  }
+  if (layer_type == "gru-insyn") {
+    return new GRULayer(layer_size, false, true);
+  }
+  if (layer_type == "gru-full") {
+    return new GRULayer(layer_size, true, true);
+  }
+  {
+    std::string prefix = "scrn";
+    std::string suffix = "fast";
+    if (layer_type.substr(0, prefix.size()) == prefix) {
+      bool fast = false;
+      int offset = prefix.size();
+      if (layer_type.substr(prefix.size(), suffix.size()) == suffix) {
+        fast = true;
+        offset += suffix.size();
+      }
+      int context_size = atoi(layer_type.substr(offset).c_str());
+      if (context_size > layer_size) {
+        fprintf(stderr, "WARNING (SCRNLayer) context size must less than or equal to layer size\n");
+        context_size = layer_size;
+      }
+      return new SCRNLayer(layer_size, context_size, !fast || !first_layer);
+    }
+  }
+  return NULL;
+}
+
+
+IRecLayer* CreateLayer(const std::string& layer_type, int layer_size, int layer_count) {
+  if (layer_count <= 0) {
+    fprintf(stderr, "ERROR layer count must be positive!\n");
+    exit(1);
+  }
+
+  std::vector<IRecLayer*> layers;
+  for (int i = 0; i < layer_count; ++i) {
+    IRecLayer* layer = CreateSingleLayer(layer_type, layer_size, (i == 0));
+    if (layer == NULL) {
+      for (int j = 0; j < i; ++j) {
+        delete layers[j];
+      }
+      return NULL;
+    }
+    layers.push_back(layer);
+  }
+
+  if (layer_count == 1) {
+    return layers[0];
+  }
+
+  return new LayerStack(layer_size, layers);
+}

faster-rnnlm/layers/interface.h
@@ -1,0 +1,129 @@
+#ifndef FASTER_RNNLM_LAYERS_INTERFACE_H_
+#define FASTER_RNNLM_LAYERS_INTERFACE_H_
+
+#include <stdio.h>
+
+#include <string>
+#include <vector>
+
+#include "faster-rnnlm/settings.h"
+#include "faster-rnnlm/util.h"
+
+class IRecLayer;
+
+// factory to create layer of given type
+// returns NULL if layer_type is invalid
+IRecLayer* CreateLayer(const std::string& layer_type, int layer_size, int layer_count);
+
+
+// Each layer must derive from IRecLayer and define two functions
+//    CreateUpdater returns a new instance of a class derived from IRecUpdater
+//    GetWeights returns a pointer to class weights (derived from IRecWeights)
+//
+// IRecWeights stands for anything that could Dump to file and Load from file
+// It also must have function DiagonalInitialization to mimic initialization
+// from http://arxiv.org/pdf/1504.00941v2.pdf
+//
+// Updaters (objects that could use weights to perform propagation)
+// - has 4 accessor methods returning state-per-timestamp matrices:
+//     GetInputMatrix()
+//       returns write-only matrix where user have to put previous hidden states (inputs)
+//     GetOutputMatrix()
+//       returns read-only with outputs of the reccurent layer
+//     GetOutputGradMatrix()
+//       returns write-only matrix where user have to put gradients
+//       with respect to output layer
+//     GetInputGradMatrix()
+//       returns read-only matrix with gradients over the GetInputMatrix()
+//
+// - must define three action methods:
+//   ForwardSequence
+//     makes a forward pass over given number of steps over GetInputMatrix() matrix
+//     fills GetOutputMatrix() matrix with per-step outputs
+//   BackwardSequence
+//     makes BPTT
+//     assumes that GetOutputGradMatrix() is filled with loss function gradients
+//     with respect to output
+//     fills GetInputGradMatrix() with loss function gradients with respect to input
+//   UpdateWeights
+//     updates layer weights
+//
+// - one should create a separate updater within each thread using layer's CreateUpdater method
+class IRecUpdater;
+class IRecWeights;
+
+class IRecLayer {
+ public:
+  virtual IRecUpdater* CreateUpdater() = 0;
+  virtual IRecWeights* GetWeights() = 0;
+  virtual ~IRecLayer() {}
+};
+
+
+class IRecUpdater {
+ public:
+  IRecUpdater(int layer_size)
+    : size_(layer_size)
+    , input_(MAX_SENTENCE_WORDS, size_)
+    , input_g_(MAX_SENTENCE_WORDS, size_)
+    , output_(MAX_SENTENCE_WORDS, size_)
+    , output_g_(MAX_SENTENCE_WORDS, size_) {}
+
+  virtual ~IRecUpdater() {}
+
+  RowMatrix& GetInputMatrix() { return input_; }
+  RowMatrix& GetInputGradMatrix() { return input_g_; }
+  RowMatrix& GetOutputMatrix() { return output_; }
+  RowMatrix& GetOutputGradMatrix() { return output_g_; }
+
+  void ForwardSequence(int steps) { return ForwardSubSequence(0, steps); }
+  void ForwardStep(int step_idx) { return ForwardSubSequence(step_idx, 1); }
+
+  virtual void BackwardSequence(int steps, uint32_t truncation_seed, int bptt_period, int bptt) = 0;
+  virtual void UpdateWeights(int steps, Real lrate, Real l2reg, Real rmsprop, Real gradient_clipping) = 0;
+  virtual void ForwardSubSequence(int start, int steps) = 0;
+
+ protected:
+  const int size_;
+
+  RowMatrix input_, input_g_;
+  RowMatrix output_, output_g_;
+};
+
+
+// IRecWeights stores a few square matrices and a few bias vectors
+// It can dump and load them from a file object
+class IRecWeights {
+ public:
+  virtual ~IRecWeights() {}
+
+  virtual void Dump(FILE* fo) const {
+    for (size_t i = 0; i < matrices_.size(); ++i) {
+      ::Dump(matrices_[i], fo);
+    }
+    for (size_t i = 0; i < vectors_.size(); ++i) {
+      ::Dump(vectors_[i], fo);
+    }
+  }
+
+  virtual void Load(FILE* fo) {
+    for (size_t i = 0; i < matrices_.size(); ++i) {
+      ::Load(&matrices_[i], fo);
+    }
+    for (size_t i = 0; i < vectors_.size(); ++i) {
+      ::Load(&vectors_[i], fo);
+    }
+  }
+
+  virtual void DiagonalInitialization(Real) = 0;
+
+ protected:
+  IRecWeights(int syn_count, int bias_count)
+      : matrices_(syn_count), vectors_(bias_count) {}
+
+  std::vector<RowMatrix> matrices_;
+  std::vector<RowVector> vectors_;
+};
+
+#endif  // FASTER_RNNLM_LAYERS_INTERFACE_H_
+

faster-rnnlm/layers/layer_stack.cc
@@ -1,0 +1,1 @@
+#include "faster-rnnlm/layers/layer_stack.h"

commit 9119842ebfea9b6c358c018f823f3d99d54405f1

Fix truncated ReLU forward pass
Bug was introduced in ce038058

faster-rnnlm/layers/activation_functions.h
@@ -2,6 +2,7 @@
 #define FASTER_RNNLM_LAYERS_ACTIVATION_FUNCTIONS_H_
 
 #include <math.h>
+#include <algorithm>
 
 #include "faster-rnnlm/settings.h"
 
@@ -72,7 +73,7 @@ struct ReLUActivation : public IActivation {
 struct TruncatedReLUActivation : public IActivation {
   void Forward(Real* hidden, int size) {
     for (int i = 0; i < size; i++) {
-      hidden[i] = (hidden[i] > 0 && hidden[i] < kReLUTruncation) ? hidden[i] : 0;
+      hidden[i] = std::min(std::max(hidden[i], static_cast<Real>(0)), kReLUTruncation);
     }
   }
 